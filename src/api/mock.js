680cb8caaf22811cc41c7724b7f5a1cf4e49e1d7
William Stam <williamstam@gmail.com>
2020-10-09 10:45:25 +0100

    [api] Add mock file with data

---
+++ src/api/mock.js
@@ -0,0 +1,75 @@
+const users = {
+  users: [
+    {
+      info: {
+        userName: 'Polarsaurus',
+      },
+      status: 'Online',
+      playing: true,
+      game: 'Borderlands 3',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'Stamboya',
+      },
+      status: 'Online',
+      playing: true,
+      game: '7 days to die',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'Trobbel',
+      },
+      status: 'Online',
+      playing: true,
+      game: 'Arma 3',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'Npahlfer',
+      },
+      status: 'Online',
+      playing: false,
+      game: '',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'Bipolar Bear @ I AM TUROK!',
+      },
+      status: 'Online',
+      playing: false,
+      game: 'Borderlands 3',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'Roger Moo',
+      },
+      status: 'away',
+      playing: false,
+      game: '',
+      lastOnline: '',
+    },
+
+    {
+      info: {
+        userName: 'maeertin',
+      },
+      status: 'Offline',
+      playing: false,
+      game: '',
+      lastOnline: '3 months ago',
+    },
+  ],
+}
+
+export default users
