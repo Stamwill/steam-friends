00986b64a1e08e2b1df200d114c6cc840e7b2279
William Stam <williamstam@gmail.com>
2020-10-14 11:47:58 +0100

    [Online] Add loop to go through friends api

--- src/online/Online.js
+++ src/online/Online.js
@@ -1,29 +1,29 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
+ import { userType } from 'utils'
+import FriendsList from '../components/FriendsList/FriendsList'
 import classes from './Online.module.css'
 
 const Online = React.forwardRef(function Online(props, ref) {
-  const { children, className, ...other } = props
-
+  const { friends, className, ...other } = props
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      <img
-        className={classes.friendImage}
-        src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg"
-        alt="friend-profile-img"
-      />
-      <div className={classes.friendContainer}>
-        <h3 className={classes.friendName}>Stamboya</h3>
-        <h2 className={classes.friendPlaying}>7 days to die</h2>
+      <div>
+        {friends.map((friend, idx) => (
+          <FriendsList key={idx} friends={friend} />
+        ))}
       </div>
+
+      <hr className={classes.onlineHr} />
     </div>
   )
 })
 
 Online.propTypes = {
-  children: PropTypes.node.isRequired,
+  friends: PropTypes.arrayOf(userType),
   className: PropTypes.string,
+
 }
 
 Online.uiName = 'Online'
