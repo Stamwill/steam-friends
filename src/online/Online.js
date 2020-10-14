c2f2dd0956584c716086e34611a3191bc452145a
William Stam <williamstam@gmail.com>
2020-10-14 16:02:08 +0100

    [Online] Remove map loop, add tags

--- src/online/Online.js
+++ src/online/Online.js
@@ -1,26 +1,31 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
-import FriendsList from '../components/FriendsList/FriendsList'
 import classes from './Online.module.css'
 
 const Online = React.forwardRef(function Online(props, ref) {
   const { friends, className, ...other } = props
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      <div>
-        {friends.map((friend, idx) => (
-          <FriendsList key={idx} friends={friend} />
-        ))}
+
+
+       <a className={classes.userImg} href="/">
+        <img src="//source.unsplash.com/45x45?gaming" alt="" />
+      </a>
+
+      <div className={classes.userContainer}>
+        <h4 className={classes.userName}>{friends.userName}</h4>
+
+        <h5 className={classes.userStatus}> {friends.userStatus} </h5>
       </div>
 
-      <hr className={classes.onlineHr} />
+
     </div>
   )
 })
 
 Online.propTypes = {
-  friends: PropTypes.array.isRequired,
+  friends: PropTypes.object.isRequired,
   className: PropTypes.string,
 
 }
