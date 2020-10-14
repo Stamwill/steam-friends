12e75569e70714078a2eb0738bb28dc99d2026cf
William Stam <williamstam@gmail.com>
2020-10-14 11:48:26 +0100

    [utils] Change userType PropTypes

--- src/utils/userType.js
+++ src/utils/userType.js
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types'
 
 const userType = PropTypes.shape({
-  info: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  playing: PropTypes.bool,
-  game: PropTypes.string,
-  lastOnline: PropTypes.string,
+  userName: PropTypes.string.isRequired,
+  userStatus: PropTypes.string.isRequired,
 })
 
 export default userType
