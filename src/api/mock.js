0f8b73f786e17c8171850f5961cb8f4357856fad
William Stam <williamstam@gmail.com>
2020-10-14 11:46:13 +0100

    [api] Add new mock data

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,75 +1,101 @@
-const users = {
-  users: [
-    {
-      info: {
-        userName: 'Polarsaurus',
-      },
-      status: 'Online',
-      playing: true,
-      game: 'Borderlands 3',
-      lastOnline: '',
-    },
+// export const friends = {
+//   users: [
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Polarsaurus',
+//       },
+//       status: 'Online',
+//       playing: true,
+//       game: 'Borderlands 3',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'Stamboya',
-      },
-      status: 'Online',
-      playing: true,
-      game: '7 days to die',
-      lastOnline: '',
-    },
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Stamboya',
+//       },
+//       status: 'Online',
+//       playing: true,
+//       game: '7 days to die',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'Trobbel',
-      },
-      status: 'Online',
-      playing: true,
-      game: 'Arma 3',
-      lastOnline: '',
-    },
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Trobbel',
+//       },
+//       status: 'Online',
+//       playing: true,
+//       game: 'Arma 3',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'Npahlfer',
-      },
-      status: 'Online',
-      playing: false,
-      game: '',
-      lastOnline: '',
-    },
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Npahlfer',
+//       },
+//       status: 'Online',
+//       playing: false,
+//       game: '',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'Bipolar Bear @ I AM TUROK!',
-      },
-      status: 'Online',
-      playing: false,
-      game: 'Borderlands 3',
-      lastOnline: '',
-    },
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Bipolar Bear @ I AM TUROK!',
+//       },
+//       status: 'Online',
+//       playing: false,
+//       game: 'Borderlands 3',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'Roger Moo',
-      },
-      status: 'away',
-      playing: false,
-      game: '',
-      lastOnline: '',
-    },
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'Roger Moo',
+//       },
+//       status: 'away',
+//       playing: false,
+//       game: '',
+//       lastOnline: '',
+//     },
 
-    {
-      info: {
-        userName: 'maeertin',
-      },
-      status: 'Offline',
-      playing: false,
-      game: '',
-      lastOnline: '3 months ago',
-    },
-  ],
-}
+//     {
+//       info: {
+//         userImg:
+//           'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b7/b75bbc8332fc6b80b85b31f8a617fdc5bfa9d6e4_full.jpg',
+//         userName: 'maeertin',
+//       },
+//       status: 'Offline',
+//       playing: false,
+//       game: '',
+//       lastOnline: '3 months ago',
+//     },
+//   ],
+// }
 
-export default users
+export const friends = [
+
+  {
+    userName: "Charlie",
+    userStatus: "Online",
+  },
+  {
+    userName: "will",
+    userStatus: "Online",
+  },
+]
+
+export default friends
