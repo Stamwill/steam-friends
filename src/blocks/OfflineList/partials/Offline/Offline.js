8db387cc8e767551b3824ffd8f1e85b780f80dcc
William <williamstam@gmail.com>
2021-01-04 15:44:00 +0000

    [Offline] Import DownArrow

--- src/blocks/OfflineList/partials/Offline/Offline.js
+++ src/blocks/OfflineList/partials/Offline/Offline.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import { userType } from 'utils/index'
+import DownArrow from '../../../../components/DownArrow/DownArrow'
 import classes from './Offline.module.css'
 
 const Offline = React.forwardRef(function Offline(props, ref) {
@@ -14,8 +15,12 @@ const Offline = React.forwardRef(function Offline(props, ref) {
       </a>
 
       <div className={classes.userContainer}>
-        <h4 className={classes.userName}> {offlines.userName}  <span className={classes.userArrow}>&or;</span></h4>
-
+        <div className={classes.friendAndOptions}>
+          <h4 className={classes.userName}>{offlines.userName}</h4>
+          <div className={classes.userArrow}>
+            <DownArrow />
+          </div>
+        </div>
         <h5 className={classes.userStatus}> {offlines.userStatus} </h5>
       </div>
     </div>
