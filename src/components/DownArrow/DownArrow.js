e083ea4c52003ae70aac4956d463054f5a236692
William <williamstam@gmail.com>
2021-01-05 14:46:55 +0000

    [DownArrow] Change into arrow function

--- src/components/DownArrow/DownArrow.js
+++ src/components/DownArrow/DownArrow.js
@@ -2,12 +2,12 @@ import * as React from 'react'
 import { AiOutlineDown } from 'react-icons/ai'
 import classes from './DownArray.module.css'
 
-const DownArrow = React.forwardRef(function DownArrow(props, ref) {
+const DownArrow = () => {
   return (
     <div className={classes.root}>
       <AiOutlineDown className={classes.downArrow} />
     </div>
   )
-})
+}
 
 export default DownArrow
