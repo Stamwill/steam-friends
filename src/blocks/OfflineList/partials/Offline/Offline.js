9710f2301d57c2b3bde4a4b71c7533f3b9741da9
William <williamstam@gmail.com>
2021-01-07 16:24:52 +0000

    [Offline] Import FriendOptions and pass props

--- src/blocks/OfflineList/partials/Offline/Offline.js
+++ src/blocks/OfflineList/partials/Offline/Offline.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import { userType } from 'utils/index'
 import OptionArrow from '../../../../components/OptionArrow'
+import FriendOptions from '../../../FriendOptions/FriendOptions'
 import classes from './Offline.module.css'
 
 const Offline = React.forwardRef(function Offline(props, ref) {
-  const { offlines, className, ...other } = props
+  const { offlines, open, toggleOption, className, ...other } = props
 
   return (
-    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+    <div className={classnames(classes.root, { [classes.open]: open }, className)} ref={ref} {...other}>
           <a className={classes.userImg} href="/">
         <img src="//source.unsplash.com/40x40?gaming" alt="" />
       </a>
@@ -18,7 +19,8 @@ const Offline = React.forwardRef(function Offline(props, ref) {
         <div className={classes.friendAndOptions}>
           <h4 className={classes.userName}>{offlines.userName}</h4>
           <div className={classes.userArrow}>
-            <OptionArrow />
+            <OptionArrow toggleOption={toggleOption}/>
+            <FriendOptions open={open}/>
           </div>
         </div>
         <h5 className={classes.userStatus}> {offlines.userStatus} </h5>
@@ -30,6 +32,8 @@ const Offline = React.forwardRef(function Offline(props, ref) {
 Offline.propTypes = {
   userType: PropTypes.arrayOf(userType),
   offlines: PropTypes.object,
+  open: PropTypes.bool,
+  toggleOption: PropTypes.func,
   className: PropTypes.string,
 }
 
