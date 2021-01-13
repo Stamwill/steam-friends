1b25aa9cf701087afc01020ab75a7f822d076655
William <williamstam@gmail.com>
2021-01-13 15:34:21 +0000

    [Online] Move mouseLeave function to container

--- src/blocks/OnlineList/partials/Online/Online.js
+++ src/blocks/OnlineList/partials/Online/Online.js
@@ -16,12 +16,12 @@ const Online = React.forwardRef(function Online(props, ref) {
         <img src="//source.unsplash.com/40x40?hiking" alt="" />
       </a>
 
-      <div className={classes.userContainer}>
+      <div className={classes.userContainer} onMouseLeave={onMouseLeave}>
         <div className={classes.friendAndOptions}>
           <h4 className={classes.userName}>{friends.userName}</h4>
           <div className={classes.userArrow}>
             <OptionArrow toggleOption={toggleOption}/>
-            <FriendOptions open={open} onMouseLeave={onMouseLeave}/>
+            <FriendOptions open={open} />
           </div>
         </div>
         <h5 className={classes.userStatus}> {friends.userStatus} </h5>
