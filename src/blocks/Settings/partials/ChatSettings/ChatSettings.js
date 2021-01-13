6e85ab4e096067538e76a35f8b8be154c5ff6746
William <williamstam@gmail.com>
2021-01-13 17:18:14 +0000

    [ChatSettings] Add placeholder tag

--- src/blocks/Settings/partials/ChatSettings/ChatSettings.js
+++ src/blocks/Settings/partials/ChatSettings/ChatSettings.js
@@ -1,12 +1,21 @@
 import * as React from 'react'
+import classnames from 'clsx'
+import PropTypes from 'prop-types'
 import classes from './ChatSettings.module.css'
 
 const ChatSettings = React.forwardRef(function ChatSettings(props, ref) {
+
+  const { open } = props 
+
   return (
-    <div className={classes.root}>
+    <div className={classnames(classes.root, {[ classes.open]: open })}>
       <h1>ChatSettings</h1>
     </div>
   )
 })
+
+ChatSettings.propTypes = {
+  open: PropTypes.bool,
+}
 
 export default ChatSettings
