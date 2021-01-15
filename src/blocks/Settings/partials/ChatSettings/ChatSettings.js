7ed8359e74c1e8755cfe94094b5d9f1890bea24c
William <williamstam@gmail.com>
2021-01-15 15:07:24 +0000

    [ChatSettings] Remove props

--- src/blocks/Settings/partials/ChatSettings/ChatSettings.js
+++ src/blocks/Settings/partials/ChatSettings/ChatSettings.js
@@ -1,14 +1,11 @@
 import * as React from 'react'
-import classnames from 'clsx'
 import PropTypes from 'prop-types'
 import classes from './ChatSettings.module.css'
 
 const ChatSettings = React.forwardRef(function ChatSettings(props, ref) {
 
-  const { open } = props 
-
   return (
-    <div className={classnames(classes.root, {[ classes.open]: open })}>
+    <div className={classes.root}>
       <h1>ChatSettings</h1>
     </div>
   )
