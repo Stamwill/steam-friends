63b4ebfa4d1ab49f5b4b4c1978764c7ab268e97a
William <williamstam@gmail.com>
2021-01-18 16:31:22 +0000

    [SizeScaleOptionsContainer] Add container partial for SizeScaleSettings

---
+++ src/blocks/Settings/partials/SizeScaleSettings/partials/SizeScaleOptionsContainer.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import OnOffButton from '../../../../../components/OnOffButton'
+import classes from './SizeScaleOptionsContainer.module.css'
+
+const SizeScaleOptionsContainer = React.forwardRef(function SizeScaleOptionsContainer(props, ref) {
+
+  const { sizeOptions } = props
+
+  return (
+    <div className={classes.root} ref={ref}>
+       <div className={classes.optionContainer}>
+         <h2>{sizeOptions.option}</h2>
+          <div className={classes.onOffButtons}>
+            <OnOffButton />
+          </div>
+        </div>
+    </div>
+  )
+})
+
+SizeScaleOptionsContainer.propTypes = {
+  sizeOptions: PropTypes.object,
+}
+
+export default SizeScaleOptionsContainer
