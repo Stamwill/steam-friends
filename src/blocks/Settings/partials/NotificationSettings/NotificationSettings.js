15f99fc7ba6708a86b34f0b2fb4d3e39209785b1
William <williamstam@gmail.com>
2021-01-18 16:40:26 +0000

    [NotificationSettings] Import container and props, Add map for Options List

--- src/blocks/Settings/partials/NotificationSettings/NotificationSettings.js
+++ src/blocks/Settings/partials/NotificationSettings/NotificationSettings.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
+import NotificationOptionsContainer from './partials/NotificationsOptionsContainer'
 import classes from './NotificationSettings.module.css'
 
 const NotificationSettings = React.forwardRef(function NotificationSettings(props, ref) {
+  
+  const { notificationOptions } = props
+  
   return (
     <div className={classes.root}>
-      <h1>NotificationSettings</h1>
+     {notificationOptions.map((option, idx) => (        
+      <NotificationOptionsContainer 
+        key={idx}
+        notificationOptions={option}
+       />
+     ))}
     </div>
   )
 })
+
+NotificationSettings.propTypes = {
+  notificationOptions: PropTypes.array,
+}
 
 export default NotificationSettings
