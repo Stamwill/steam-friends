8fcd409145213675340815f979ef9199cccfdb42
William <williamstam@gmail.com>
2021-01-18 16:41:02 +0000

    [SizeScaleSettings] Import props and container, add map for options list

--- src/blocks/Settings/partials/SizeScaleSettings/SizeScaleSettings.js
+++ src/blocks/Settings/partials/SizeScaleSettings/SizeScaleSettings.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
+import SizeScaleOptionsContainer from './partials'
 import classes from './SizeScaleSettings.module.css'
 
 const SizeScaleSettings = React.forwardRef(function SizeScaleSettings(props, ref) {
+
+  const { sizeOptions } = props
+
   return (
     <div className={classes.root}>
-      <h1>SizeScaleSettings</h1>
+      {sizeOptions.map((option, idx) => (
+        <SizeScaleOptionsContainer 
+          key={idx}
+          sizeOptions={option}
+        />
+      ))}
     </div>
   )
 })
+
+SizeScaleSettings.propTypes = {
+  sizeOptions: PropTypes.array,
+}
 
 export default SizeScaleSettings
