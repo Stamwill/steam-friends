dff6bad50b9c6f2b13841df1f1cb0284f229a590
William <williamstam@gmail.com>
2021-01-18 16:39:03 +0000

    [ChatSettings] import container, add map for list of options

--- src/blocks/Settings/partials/ChatSettings/ChatSettings.js
+++ src/blocks/Settings/partials/ChatSettings/ChatSettings.js
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import ChatOptionsContainer from './partials/ChatOptionsContainer'
 import classes from './ChatSettings.module.css'
 
 const ChatSettings = React.forwardRef(function ChatSettings(props, ref) {
+  const { chatOptions } = props
 
   return (
-    <div className={classes.root}>
-      <h1>ChatSettings</h1>
+    <div className={classes.root} ref={ref} >
+      {chatOptions.map((option, idx) => (
+         <ChatOptionsContainer 
+         key={idx}
+         chatOptions={option}
+       />
+      ))}
     </div>
   )
 })
 
 ChatSettings.propTypes = {
-  open: PropTypes.bool,
+  chatOptions: PropTypes.array
 }
 
 export default ChatSettings
