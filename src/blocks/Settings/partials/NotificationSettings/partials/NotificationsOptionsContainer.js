fb9539cf3d28d03052d468e1a09c11f580b88a87
William <williamstam@gmail.com>
2021-01-18 16:30:51 +0000

    [NotificationOptionsContainer] Add container partial for NotificationOptionsContainer

---
+++ src/blocks/Settings/partials/NotificationSettings/partials/NotificationsOptionsContainer.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import OnOffButton from '../../../../../components/OnOffButton'
+import classes from './NotificationsOptionsContainer.module.css'
+
+const NotificationOptionsContainer = React.forwardRef(function NotificationOptionsContainer(props, ref) {
+
+  const { notificationOptions } = props
+
+  return (
+    <div className={classes.root} ref={ref}>
+       <div className={classes.optionContainer}>
+         <h2>{notificationOptions.option}</h2>
+          <div className={classes.onOffButtons}>
+            <OnOffButton />
+          </div>
+        </div>
+    </div>
+  )
+})
+
+NotificationOptionsContainer.propTypes = {
+  notificationOptions: PropTypes.object,
+}
+
+export default NotificationOptionsContainer
