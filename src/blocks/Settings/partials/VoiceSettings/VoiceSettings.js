eae311162343bf0bc26a8c56774b753319bfe831
William <williamstam@gmail.com>
2021-01-19 16:23:52 +0000

    [VoiceSettings] Add label for options

--- src/blocks/Settings/partials/VoiceSettings/VoiceSettings.js
+++ src/blocks/Settings/partials/VoiceSettings/VoiceSettings.js
@@ -3,8 +3,22 @@ import classes from './VoiceSettings.module.css'
 
 const VoiceSettings = React.forwardRef(function VoiceSettings(props, ref) {
   return (
-    <div className={classes.root}>
-      <h1>VoiceSettings</h1>
+    <div className={classes.root} ref={ref}>
+      <h1 className={classes.header}>VOICE</h1>
+      <div className={classes.micTestContainer}>
+        <div className={classes.micTest}>
+          <button className={classes.button} type="submit">Start Microphone Test</button>
+        </div>
+      </div>
+
+      <div className={classes.voiceInpDev}>
+        <label htmlFor="Voice Input Device">VOICE INPUT DEVICE</label>
+        <select>
+          <option>Default</option>
+          <option>Headphone</option>
+          <option>Speakers</option>
+        </select>
+      </div>
     </div>
   )
 })
