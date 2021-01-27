20264faf8cb0e1ce2f32b46122fe8f45cae308bd
William <williamstam@gmail.com>
2021-01-27 14:55:03 +0000

    [OnOffButton] Add state to toggle buttons on/off

--- src/components/OnOffButton/OnOffButton.js
+++ src/components/OnOffButton/OnOffButton.js
@@ -1,14 +1,23 @@
 import * as React from 'react'
+import classnames from 'clsx'
 import classes from './OnOffButton.module.css'
 
 const OnOffButton = React.forwardRef(function OnOffButton(props, ref) {
 
+  const [isOn, setIsOn] = React.useState(false)
+  const [isOff, setIsOff] = React.useState(true)
+
+  const handleClick = () => {
+    setIsOn((prevState) => !prevState)
+    setIsOff((prevState) => !prevState)
+  }
+
 
   return (
-    <div className={classes.root} ref={ref}>
-      <div className={classes.buttons}>
-        <button className={classes.buttonOff} type="submit">Off</button>
-        <button className={classes.buttonOn} type="submit">On</button>
+    <div className={classnames(classes.root)} ref={ref} >
+      <div className={classes.buttons} onClick={handleClick}>
+        <button className={classnames(classes.buttonOff, {[classes.off]: isOff})} type="submit">Off</button>
+        <button className={classnames(classes.buttonOn, {[classes.open]: isOn})} type="submit">On</button>
       </div>
     </div>
   )
