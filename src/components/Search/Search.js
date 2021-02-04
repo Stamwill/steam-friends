9f6e1efb5c967cbaed5a75c69393626004f9efab
William <williamstam@gmail.com>
2021-02-04 15:57:23 +0000

    [Search] Add test function

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -8,34 +8,52 @@ import classes from './Search.module.css'
 const Search = React.forwardRef(function Search(props, ref) {
   const {  className, handleSearch, ...other } = props
 
+  const [showGlass, setShowGlass] = React.useState(false)
+
+  const test = () => {
+    console.log('test1', 'test2')
+  }
+
+  const handleGlass = () => {
+    if (showGlass) {
+      setShowGlass((prevState) => !prevState)
+      document.addEventListener('click', test )
+    } else {
+      document.addEventListener('click', test)
+    }
+  }
+
+
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      
-        <label className={classes.searchLabel} name="friends">
-          Friends
-        </label>
+    
+      <label className={classes.searchLabel} name="friends">
+        Friends
+      </label>
 
-        <div className={classes.searchField}>
-          <input 
-            className={classes.searchInput} 
-            type="search" name="friends" 
-            placeholder="search friends" 
-            onChange={handleSearch}
-          />
-          <AiOutlineSearch className={classes.magnifying}/>
-        </div>
-
-        <div className={classes.searchIcons}>
-          <HiUserAdd className={classes.add}/>
-        </div>
+      <div className={classes.searchField}>
+        <input 
+          className={classes.searchInput} 
+          type="search" name="friends" 
+          placeholder="search friends" 
+          onChange={handleSearch}
+          onClick={handleGlass}
+        />
       </div>
- 
+        <AiOutlineSearch className={classnames(classes.magnifying, {[classes.glass]: showGlass})} />
+
+      <div className={classes.searchIcons}>
+        <HiUserAdd className={classes.add}/>
+      </div>
+    </div>
   )
 })
 
 Search.propTypes = {
   className: PropTypes.string,
   handleSearch: PropTypes.func,
+  handleGlass: PropTypes.func,
+  showGlass: PropTypes.bool,
 }
 
 Search.uiName = 'Search'
