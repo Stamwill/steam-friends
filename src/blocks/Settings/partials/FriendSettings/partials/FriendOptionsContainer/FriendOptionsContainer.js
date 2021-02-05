9aab8e7b67048b99af694536fdfe5b57af8dac5e
William <williamstam@gmail.com>
2021-02-05 13:50:46 +0000

    [FriendOptionsContainer] Add props

--- src/blocks/Settings/partials/FriendSettings/partials/FriendOptionsContainer/FriendOptionsContainer.js
+++ src/blocks/Settings/partials/FriendSettings/partials/FriendOptionsContainer/FriendOptionsContainer.js
@@ -5,14 +5,14 @@ import classes from './FriendOptionsContainer.module.css'
 
 const FriendOptionsContainer = React.forwardRef(function FriendOptionsContainer(props, ref) {
 
-  const { friendOptions } = props
+  const { friendOptions, isOn, isOff, handleClick } = props
   
   return (
     <div className={classes.root} ref={ref}>
        <div className={classes.optionContainer}>
          <h2 className={classes.option}>{friendOptions.option}</h2>
           <div className={classes.onOffButtons}>
-            <OnOffButton />
+            <OnOffButton isOn={isOn} isOff={isOff} handleClick={handleClick}/>
           </div>
         </div>
     </div>
