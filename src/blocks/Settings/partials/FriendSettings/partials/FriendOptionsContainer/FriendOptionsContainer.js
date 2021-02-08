765bb765a5ca0b1c745d249344d57059a10bfbc4
William <williamstam@gmail.com>
2021-02-08 14:35:52 +0000

    [FriendOptionsContainer] Remove isOff props

--- src/blocks/Settings/partials/FriendSettings/partials/FriendOptionsContainer/FriendOptionsContainer.js
+++ src/blocks/Settings/partials/FriendSettings/partials/FriendOptionsContainer/FriendOptionsContainer.js
@@ -5,14 +5,14 @@ import classes from './FriendOptionsContainer.module.css'
 
 const FriendOptionsContainer = React.forwardRef(function FriendOptionsContainer(props, ref) {
 
-  const { friendOptions, isOn, isOff, handleClick } = props
+  const { friendOptions, isOn, handleClick } = props
   
   return (
     <div className={classes.root} ref={ref}>
        <div className={classes.optionContainer}>
          <h2 className={classes.option}>{friendOptions.option}</h2>
           <div className={classes.onOffButtons}>
-            <OnOffButton isOn={isOn} isOff={isOff} handleClick={handleClick}/>
+            <OnOffButton isOn={isOn} handleClick={handleClick}/>
           </div>
         </div>
     </div>
