ef1a1d946dd859376b97e85264fadc2b6c6cb037
William <williamstam@gmail.com>
2021-02-17 17:52:34 +0000

    [client] Add client for api

---
+++ src/api/client.js
@@ -0,0 +1,39 @@
+class ApiClient { 
+  static baseURL = "/graphql/"
+
+  async request(query, variables = {}) {
+    let res = {}
+    
+    try {
+      const response = await fetch(this.baseURL, { 
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          query, variables
+        })      
+      })
+      res = await response.json()
+
+    } catch(error) {
+      console.log('API client request error: ' , error)
+    }
+
+    return res.data
+  }
+  
+  async getFriends() {
+    const query = `query {
+      friends {
+        userName,
+        userStatus,
+      }
+    }`
+    return this.request(query)
+  }
+}
+
+const client = new ApiClient()
+export default client
